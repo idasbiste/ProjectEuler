14309092d7d723d38f19fd5361ddffd20eb36313
Eduardo Fernandes <efernandes@MBP-de-Eduardo.home>
2016-03-06 02:07:39 +0000

    Remove test content

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ProjectEuler
 Solutions in Python for Project Euler exercises.
 Trying to improve solutions as times go by...
-
-Modifying for pulling contente
--- ex1.py
+++ ex1.py
@@ -1,4 +1,11 @@
+import timingDecorator
+
+@profile
 def isMultipleOfThreeOrFive(n):
 	return n % 3 == 0 or n % 5 == 0
 
-print sum([n for n in range(1,1000) if isMultipleOfThreeOrFive(n)])
+@profile
+def ex1():
+	print sum([n for n in range(1,1000) if isMultipleOfThreeOrFive(n)])
+
+ex1()
